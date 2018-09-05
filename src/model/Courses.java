c51c2924b562b1bf64f569dbc12a9b6948c1cd87
Orvin Savero <orvinsavero@yahoo.com>
2018-09-05 11:04:01 +0700

    abc

--- src/model/Courses.java
+++ src/model/Courses.java
@@ -13,6 +13,7 @@ public class Courses {
     private Integer courseId;
     private String courseName;
     private String courseDescription;
+    private String abc;
 
     /**
      * @return the courseId
